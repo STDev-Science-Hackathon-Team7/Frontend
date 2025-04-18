5736486f0dc46f2808c171b1eca771dc04ddf83b
Monixc <chris13404@gmail.com>
2025-04-18 20:12:52 +0900

    style: pretendard 폰트 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Pretendard"]
+			},
+			fontWeight: {
+				normal: 400,
+				medium: 500,
+				semibold: 600,
+				bold: 700,
+				extrabold: 800,
+				black: 900
+			}
+		}
+	},
+	plugins: []
+};
