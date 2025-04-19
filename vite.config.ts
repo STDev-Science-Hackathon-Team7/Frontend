98cec6bbb811e4efdfa15573f45d99cacb88449a
Monixc <chris13404@gmail.com>
2025-04-20 06:21:41 +0900

    build: pwa 최대 캐시 크기 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -67,6 +67,7 @@ export default defineConfig({
 				]
 			},
 			workbox: {
+				maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB로 증가
 				runtimeCaching: [
 					{
 						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
