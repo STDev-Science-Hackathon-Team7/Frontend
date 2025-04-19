1335949406dddec39497ad4ea82ca178f9f270e8
Monixc <chris13404@gmail.com>
2025-04-19 14:15:01 +0900

    fix: conflict 문제 해결

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
 declare module "*.svg?react" {
-	import * as React from 'react';
+	import * as React from "react";
 	const Component: React.FC<React.SVGProps<SVGSVGElement>>;
 	export default Component;
 }
-
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-
+import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
 		tailwindcss(),
-
+		svgr(),
 		//vitePWA 설정
 		VitePWA({
 			registerType: "autoUpdate",
