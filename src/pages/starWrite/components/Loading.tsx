5f8034760e5fd9bd2309d4a5419ae9fc2332a39f
Hina0222 <hina0222@naver.com>
2025-04-19 15:35:34 +0900

    feat: Loading 컴포넡느 lottie 이미지 추가

--- src/pages/starWrite/components/Loading.tsx
+++ src/pages/starWrite/components/Loading.tsx
@@ -1,8 +1,12 @@
+import Lottie from "lottie-react";
+import LoadingIcon from "@/assets/icons/LoadingIcon.json";
+
 export default function Loading() {
 	return (
 		<div className="min-h-screen flex flex-col justify-center items-center gap-2.5">
 			<h1 className="font-bold text-lg">분석중이에요</h1>
 			<span className="text-sm text-gray-sub">뭐를 바탕으로 분석중입니다</span>
+			<Lottie animationData={LoadingIcon} loop className="w-1/2 h-1/2"/>
 		</div>
 	);
 }
